a19bc20d4379fff0145ebaac4d019c7b43dc389f
Olga Latkina <olga.latkina@gmail.com>
2020-10-05 13:37:26 +0300

    feat: split into modules

--- js/main.js
+++ js/main.js
@@ -1,89 +1,10 @@
 "use strict";
 
-const OFFER_TYPES = {
-  bungalow: {
-    minPrice: 0,
-    translate: `Бунгало`
-  },
-  flat: {
-    minPrice: 1000,
-    translate: `Квартира`
-  },
-  house: {
-    minPrice: 5000,
-    translate: `Дом`
-  },
-  palace: {
-    minPrice: 10000,
-    translate: `Дворец`
-  }
-};
-const FEATURES = [
-  `wifi`,
-  `dishwasher`,
-  `parking`,
-  `washer`,
-  `elevator`,
-  `conditioner`,
-];
-const PHOTOS = [
-  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
-];
-const TIMES = [`12:00`, `13:00`, `14:00`];
-const ROOMS = 5;
-const GUESTS = 10;
-const PRICE = 10000;
-const fishingText = `Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit`;
-
-const MAP_MIN_Y = 130;
-const MAP_MAX_Y = 630;
-const PIN_WIDTH = 50;
-const PIN_HEIGHT = 70;
-const PINS_QUANTITY = 8;
-const MAIN_PIN = {
-  width: 62,
-  height: 62,
-  legHeight: 22,
-};
-
 const map = document.querySelector(`.map`);
 const mapPins = map.querySelector(`.map__pins`);
 const filterContainer = map.querySelector(`.map__filters-container`);
 
-const pinTemplate = document
-  .querySelector(`#pin`)
-  .content.querySelector(`.map__pin`);
-const cardTemplate = document
-  .querySelector(`#card`)
-  .content.querySelector(`.map__card`);
-
-const mapMinX = 0;
 const mapMaxX = mapPins.clientWidth;
-
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-};
-
-const getRandomInRange = (min, max) => {
-  return Math.floor(min + Math.random() * (max + 1 - min));
-};
-
-const getRandomElement = (array) => {
-  return array[Math.floor(Math.random() * array.length)];
-};
-
-const getRandomArray = (array) => {
-  const copy = [...array];
-  return shuffleArray(copy).splice(0, getRandomInRange(1, copy.length));
-};
 
 const getUrl = (num) => {
   if (num < 10) {
@@ -92,27 +13,10 @@ const getUrl = (num) => {
   return `img/avatars/user${num}.png`;
 };
 
-const createTitle = (str) => {
-  const temp = str.replace(/,*\s/g, `,`).split(`,`);
-  return getRandomElement(temp).toUpperCase();
-};
-
-const getDescription = (str) => {
-  const temp = str.split(` `);
-  const newStr = shuffleArray(temp)
-    .splice(0, getRandomInRange(4, temp.length))
-    .join(` `);
-  const firstCapitalStr = newStr.replace(newStr[0], newStr[0].toUpperCase());
-  if (firstCapitalStr[firstCapitalStr.length - 1] === `,`) {
-    return `${firstCapitalStr.slice(0, -1)}.`;
-  }
-  return `${firstCapitalStr}.`;
-};
-
 const createAdvertisingObj = (index) => {
   const coord = {
-    x: getRandomInRange(mapMinX, mapMaxX),
-    y: getRandomInRange(MAP_MIN_Y, MAP_MAX_Y),
+    x: window.utils.getRandomInRange(window.data.MAP_MIN_X, mapMaxX),
+    y: window.utils.getRandomInRange(window.data.MAP_MIN_Y, window.data.MAP_MAX_Y),
   };
 
   return {
@@ -120,21 +24,21 @@ const createAdvertisingObj = (index) => {
       avatar: getUrl(index),
     },
     offer: {
-      title: createTitle(fishingText),
+      title: window.utils.createTitle(window.data.fishingText),
       address: `${coord.x}, ${coord.y}`,
-      price: getRandomInRange(0, PRICE),
-      type: getRandomElement(Object.keys(OFFER_TYPES)),
-      rooms: getRandomInRange(0, ROOMS),
-      guests: getRandomInRange(1, GUESTS),
-      checkin: getRandomElement(TIMES),
-      checkout: getRandomElement(TIMES),
-      features: getRandomArray(FEATURES),
-      description: getDescription(fishingText),
-      photos: getRandomArray(PHOTOS),
+      price: window.utils.getRandomInRange(0, window.data.PRICE),
+      type: window.utils.getRandomElement(Object.keys(window.data.OFFER_TYPES)),
+      rooms: window.utils.getRandomInRange(0, window.data.ROOMS),
+      guests: window.utils.getRandomInRange(1, window.data.GUESTS),
+      checkin: window.utils.getRandomElement(window.data.TIMES),
+      checkout: window.utils.getRandomElement(window.data.TIMES),
+      features: window.utils.getRandomArray(window.data.FEATURES),
+      description: window.utils.getDescription(window.data.fishingText),
+      photos: window.utils.getRandomArray(window.data.PHOTOS),
     },
     location: {
-      x: coord.x - PIN_WIDTH / 2,
-      y: coord.y - PIN_HEIGHT,
+      x: coord.x - window.data.PIN_WIDTH / 2,
+      y: coord.y - window.data.PIN_HEIGHT,
     },
   };
 };
@@ -149,70 +53,7 @@ const getPinsArray = (quantity) => {
 
   return pinsArray;
 };
-
-const pinsDataArray = getPinsArray(PINS_QUANTITY);
-
-const createPinElement = (obj) => {
-  const pin = pinTemplate.cloneNode(true);
-  pin.style = `left: ${obj.location.x}px; top: ${obj.location.y}px`;
-  const img = pin.querySelector(`img`);
-  img.src = obj.author.avatar;
-  img.alt = obj.offer.title;
-  return pin;
-};
-
-const createCardElement = (obj) => {
-  const card = cardTemplate.cloneNode(true);
-  card.querySelector(`.popup__avatar`).src = obj.author.avatar;
-  card.querySelector(`.popup__title`).textContent = obj.offer.title;
-  card.querySelector(`.popup__text--address`).textContent = obj.offer.address;
-  card.querySelector(`.popup__text--price`).textContent = obj.offer.price;
-  card.querySelector(`.popup__text--capacity`)
-    .textContent = `${obj.offer.rooms} комнаты для ${obj.offer.guests} гостей`;
-  card.querySelector(`.popup__type`).textContent = obj.offer.type;
-  card.querySelector(`.popup__text--time`)
-    .textContent = `Заезд после ${obj.offer.checkin}, выезд\u00A0до ${obj.offer.checkout}`;
-
-  const features = card.querySelectorAll(`.popup__feature`);
-
-  for (let feature of features) {
-    const str = feature.className.match(/--(\w+)/)[1];
-    if (!obj.offer.features.includes(str)) {
-      feature.classList.add(`hidden`);
-    }
-  }
-
-  card.querySelector(`.popup__description`).textContent = obj.offer.description;
-
-  if (obj.offer.photos.length > 1) {
-    const imgContainer = card.querySelector(`.popup__photos`);
-    const img = card.querySelector(`.popup__photo`);
-
-    obj.offer.photos.forEach((photo, i) => {
-      if (i === 0) {
-        card.querySelector(`.popup__photo`).src = photo;
-      } else {
-        const newImg = img.cloneNode(true);
-        newImg.src = photo;
-        imgContainer.append(newImg);
-      }
-    });
-  } else {
-    card.querySelector(`.popup__photo`).src = obj.offer.photos[0];
-  }
-
-  return card;
-};
-
-const createFragment = (array, callback) => {
-  const fragment = document.createDocumentFragment();
-  array.forEach((el, i) => {
-    const element = callback(el);
-    element.dataset.id = i;
-    fragment.append(element);
-  });
-  return fragment;
-};
+const pinsDataArray = getPinsArray(window.data.PINS_QUANTITY);
 
 const adForm = document.querySelector(`.ad-form`);
 const fieldsets = adForm.querySelectorAll(`fieldset`);
@@ -254,12 +95,12 @@ const removeDisabled = (collection) => {
 
 const activatePage = () => {
   map.classList.remove(`map--faded`);
-  const pinsFragment = createFragment(pinsDataArray, createPinElement);
+  const pinsFragment = window.utils.createFragment(pinsDataArray, window.pin.createPinElement);
   mapPins.append(pinsFragment);
   adForm.classList.remove(`ad-form--disabled`);
   removeDisabled(fieldsets);
   removeDisabled(mapSelects);
-  updateCoordinate(pinMain, address, MAIN_PIN);
+  updateCoordinate(pinMain, address, window.data.MAIN_PIN);
 };
 
 const onPinMainMousedown = (evt) => {
@@ -284,10 +125,11 @@ let currentCard = null;
 let isPin = false;
 
 const showCard = (id) => {
-  const card = createCardElement(pinsDataArray[id]);
+  const card = window.card.createCardElement(pinsDataArray[id]);
   map.insertBefore(card, filterContainer);
   currentCard = card;
 
+  document.addEventListener(`keydown`, onEscPress);
   const closeCardButton = currentCard.querySelector(`.popup__close`);
   closeCardButton.addEventListener(`click`, onCardCloseClick);
 };
@@ -299,16 +141,27 @@ const onPinClick = (evt) => {
     return;
   }
 
-  const id = pin.dataset.id;
   isPin = true;
+  const id = pin.dataset.id;
   showCard(id);
 };
 
-const onCardCloseClick = (evt) => {
-  const card = evt.target.closest(`.map__card`);
-  card.remove();
+const removeCard = () => {
+  currentCard.remove();
   currentCard = null;
   isPin = false;
+};
+
+const onEscPress = (evt) => {
+  evt.preventDefault();
+  if (evt.key === `Escape`) {
+    removeCard();
+  }
+  document.removeEventListener(`keydown`, onEscPress);
+};
+
+const onCardCloseClick = () => {
+  removeCard();
 };
 
 map.addEventListener(`click`, onPinClick);
@@ -321,7 +174,7 @@ const departure = adForm.querySelector(`#timeout`);
 
 const hasTypeSelect = () => {
   const type = housingType.value;
-  const newValue = OFFER_TYPES[type].minPrice;
+  const newValue = window.data.OFFER_TYPES[type];
   price.min = newValue;
   price.placeholder = newValue;
 };
