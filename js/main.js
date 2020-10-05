6bb0e96f45750f50dbb3fc13c4deca9f588b5c12
Olga Latkina <olga.latkina@gmail.com>
2020-10-05 15:18:03 +0300

    feat: add map and form modules

--- js/main.js
+++ js/main.js
@@ -2,73 +2,20 @@
 
 const map = document.querySelector(`.map`);
 const mapPins = map.querySelector(`.map__pins`);
-const filterContainer = map.querySelector(`.map__filters-container`);
-
-const mapMaxX = mapPins.clientWidth;
-
-const getUrl = (num) => {
-  if (num < 10) {
-    return `img/avatars/user0${num}.png`;
-  }
-  return `img/avatars/user${num}.png`;
-};
-
-const createAdvertisingObj = (index) => {
-  const coord = {
-    x: window.utils.getRandomInRange(window.data.MAP_MIN_X, mapMaxX),
-    y: window.utils.getRandomInRange(window.data.MAP_MIN_Y, window.data.MAP_MAX_Y),
-  };
-
-  return {
-    author: {
-      avatar: getUrl(index),
-    },
-    offer: {
-      title: window.utils.createTitle(window.data.fishingText),
-      address: `${coord.x}, ${coord.y}`,
-      price: window.utils.getRandomInRange(0, window.data.PRICE),
-      type: window.utils.getRandomElement(Object.keys(window.data.OFFER_TYPES)),
-      rooms: window.utils.getRandomInRange(0, window.data.ROOMS),
-      guests: window.utils.getRandomInRange(1, window.data.GUESTS),
-      checkin: window.utils.getRandomElement(window.data.TIMES),
-      checkout: window.utils.getRandomElement(window.data.TIMES),
-      features: window.utils.getRandomArray(window.data.FEATURES),
-      description: window.utils.getDescription(window.data.fishingText),
-      photos: window.utils.getRandomArray(window.data.PHOTOS),
-    },
-    location: {
-      x: coord.x - window.data.PIN_WIDTH / 2,
-      y: coord.y - window.data.PIN_HEIGHT,
-    },
-  };
-};
-
-const getPinsArray = (quantity) => {
-  let pinsArray = [];
-
-  for (let i = 1; i <= quantity; i++) {
-    const offer = createAdvertisingObj(i);
-    pinsArray.push(offer);
-  }
-
-  return pinsArray;
-};
-const pinsDataArray = getPinsArray(window.data.PINS_QUANTITY);
-
-const adForm = document.querySelector(`.ad-form`);
-const fieldsets = adForm.querySelectorAll(`fieldset`);
 const mapForm = document.querySelector(`.map__filters`);
 const mapSelects = mapForm.querySelectorAll(`[id^="housing-"]`);
 const pinMain = map.querySelector(`.map__pin--main`);
-const address = adForm.querySelector(`#address`);
+const adForm = document.querySelector(`.ad-form`);
+const adFielsets = adForm.querySelectorAll(`fieldset`);
+const adAddress = adForm.querySelector(`#address`);
 
 // ------------ Disabled Elements
-const addDisabled = (collection) => {
+const setDisabled = (collection) => {
   collection.forEach((elem) => elem.setAttribute(`disabled`, ``));
 };
 
-addDisabled(fieldsets);
-addDisabled(mapSelects);
+setDisabled(adFielsets);
+setDisabled(mapSelects);
 
 // ------------ Set Coordinates
 const setCoordinate = (elem, input) => {
@@ -78,7 +25,7 @@ const setCoordinate = (elem, input) => {
 
   input.value = `${x} ${y}`;
 };
-setCoordinate(pinMain, address);
+setCoordinate(pinMain, adAddress);
 
 const updateCoordinate = (elem, input, obj) => {
   let coords = elem.getBoundingClientRect();
@@ -95,12 +42,12 @@ const removeDisabled = (collection) => {
 
 const activatePage = () => {
   map.classList.remove(`map--faded`);
-  const pinsFragment = window.utils.createFragment(pinsDataArray, window.pin.createPinElement);
+  const pinsFragment = window.utils.createFragment(window.map.pinsDataArray, window.pin.createPinElement);
   mapPins.append(pinsFragment);
   adForm.classList.remove(`ad-form--disabled`);
-  removeDisabled(fieldsets);
+  removeDisabled(adFielsets);
   removeDisabled(mapSelects);
-  updateCoordinate(pinMain, address, window.data.MAIN_PIN);
+  updateCoordinate(pinMain, adAddress, window.data.MAIN_PIN);
 };
 
 const onPinMainMousedown = (evt) => {
@@ -119,79 +66,3 @@ const onPinMainKeydown = (evt) => {
 
 pinMain.addEventListener(`mousedown`, onPinMainMousedown);
 pinMain.addEventListener(`keydown`, onPinMainKeydown);
-
-// ------------ Render Cards
-let currentCard = null;
-let isPin = false;
-
-const showCard = (id) => {
-  const card = window.card.createCardElement(pinsDataArray[id]);
-  map.insertBefore(card, filterContainer);
-  currentCard = card;
-
-  document.addEventListener(`keydown`, onEscPress);
-  const closeCardButton = currentCard.querySelector(`.popup__close`);
-  closeCardButton.addEventListener(`click`, onCardCloseClick);
-};
-
-const onPinClick = (evt) => {
-  const pin = evt.target.closest(`.map__pin:not(.map__pin--main)`);
-
-  if (!pin || isPin) {
-    return;
-  }
-
-  isPin = true;
-  const id = pin.dataset.id;
-  showCard(id);
-};
-
-const removeCard = () => {
-  currentCard.remove();
-  currentCard = null;
-  isPin = false;
-};
-
-const onEscPress = (evt) => {
-  evt.preventDefault();
-  if (evt.key === `Escape`) {
-    removeCard();
-  }
-  document.removeEventListener(`keydown`, onEscPress);
-};
-
-const onCardCloseClick = () => {
-  removeCard();
-};
-
-map.addEventListener(`click`, onPinClick);
-
-// ------------ Form Validation
-const housingType = adForm.querySelector(`#type`);
-const price = adForm.querySelector(`#price`);
-const arrival = adForm.querySelector(`#timein`);
-const departure = adForm.querySelector(`#timeout`);
-
-const hasTypeSelect = () => {
-  const type = housingType.value;
-  const newValue = window.data.OFFER_TYPES[type];
-  price.min = newValue;
-  price.placeholder = newValue;
-};
-
-// const hasPrice = () => {
-//   console.log(price.value);
-// };
-
-const changeTimeDeparture = () => {
-  arrival.value = departure.value;
-};
-
-const changeTimeArrival = () => {
-  departure.value = arrival.value;
-};
-
-housingType.addEventListener(`change`, hasTypeSelect);
-// price.addEventListener(`input`, hasPrice);
-arrival.addEventListener(`change`, changeTimeArrival);
-departure.addEventListener(`change`, changeTimeDeparture);
